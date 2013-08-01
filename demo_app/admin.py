a539041b004f17baaf78a66deb76b6afdcde793d
balu <balu@agiliq.com>
2013-08-02 02:17:47 +0530

    added models and registered models in admin

---
+++ demo_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Product, Property
+
+
+class PropertiesInline(admin.StackedInline):
+    model = Property
+    extra = 4
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PropertiesInline, ]
+
+
+admin.site.register(Product)
--- demo_app/models.py
+++ demo_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Property(models.Model):
+    name = models.CharField(verbose_name=u'Name of the Property',
+                            max_length=128)
+    value = models.CharField(verbose_name=u'Value of the Property',
+                             max_length=256)
+
+    class Meta:
+        verbose_name = _('Property')
+        verbose_name_plural = _('Properties')
+
+    def __unicode__(self):
+        pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    properties = models.ManyToManyField("Property",
+                                        related_name='product_properties')
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+    def __unicode__(self):
+        pass
