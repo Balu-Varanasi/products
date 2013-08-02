e9566f67f43a9e98530c8091194d592d3ea5db54
balu <balu@agiliq.com>
2013-08-02 20:54:12 +0530

    added a view and modified urls

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+
+from django.shortcuts import render
+
+
+def home(request, template_name='home.html'):
+    """ This renders the home page"""
+    context = {}
+    return render(request, template_name, context)
