98b941f0e1f27a515d40279e411620f13b53704c
balu <balu@agiliq.com>
2013-08-02 22:08:00 +0530

    removed product index

--- demo_app/search_indexes.py
+++ demo_app/search_indexes.py
@@ -1,5 +1,5 @@
 from haystack import indexes
-from .models import Property, Product
+from .models import Property
 
 
 class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
@@ -10,18 +10,6 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Property
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.all()
-
-
-class ProductIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name')
-
-    def get_model(self):
-        return Product
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
