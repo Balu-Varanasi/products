1da5d7a291972f4729c886b1bcd02c291c6f422e
balu <balu@agiliq.com>
2013-08-02 20:32:35 +0530

    added haystack to installed apps

--- products/settings.py
+++ products/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'demo_app',
+    'haystack',
 )
 
 # A sample logging configuration. The only tangible logging
