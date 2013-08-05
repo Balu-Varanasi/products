f8adf2ddd2d3afa1f7225c67312d891609031b6d
balu <balu@agiliq.com>
2013-08-05 05:49:31 +0530

    removed haystack urls

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, include, url
-
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
 
+
+admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'', include("demo_app.urls")),
-                       url(r'^search/', include('haystack.urls')),
                        url(r'^admin/', include(admin.site.urls)))
